a317bb28251d620bab2faf0efe387aadc9764954
Esteban Hormazabal <esteban.hormazabal.ch@gmail.com>
2022-06-08 20:13:01 -0400

    Desafio completo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<form @submit.prevent="agregarNuevaTarea" class="d-flex align-items-center">
+		<div class="d-flex col-4 align-items-center">
+			<label for="exampleInput" class="form-label">Tareas</label>
+			<input
+				v-model="nuevaTarea"
+				type="text"
+				class="form-control"
+				id="exampleInput"
+				aria-describedby="emailHelp"
+			/>
+		</div>
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+  <div v-if="tareasNuevas[0]">
+
+    <TareaComponent v-for="(tarea, $index) in tareasNuevas" :key="$index" :tarea="tarea" @eliminar:tarea="eliminarTarea($index)"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TareaComponent from './components/TareaComponent.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data: () => ({
+		nuevaTarea: "",
+		tareasNuevas: [],
+		tareasCompletadas: [],
+	}),
+	components: { TareaComponent, },
+	methods: {
+		agregarNuevaTarea() {
+			this.tareasNuevas.unshift(this.nuevaTarea);
+			this.nuevaTarea = "";
+		},
+    eliminarTarea(index) {
+      this.tareasNuevas.splice(index, 1)
+    },
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/TareaComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex" >
+    <p>{{ tarea }}</p>
+    <button class="btn btn-primary" @click="eliminarTarea">Eliminar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TareaComponent',
+  data: () => ({
+  }),
+  emits: ['eliminar:tarea'],
+  props: {
+    tarea: String
+  },
+  methods: { 
+    eliminarTarea: function (e) {
+      this.$emit('eliminar:tarea', e)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
